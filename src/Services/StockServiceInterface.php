400781c1176dc45caf39c6749684a5327d809893
Wouter <wouter@etrias.nl>
2018-05-28 09:25:57 +0200

    add new stock api listVariants

--- src/Services/StockServiceInterface.php
+++ src/Services/StockServiceInterface.php
@@ -41,6 +41,27 @@ interface StockServiceInterface
     );
 
     /**
+     * @param string[]|null $articleCodes
+     * @param string|null   $articleDescription
+     * @param DateTime|null $updatedAfter
+     * @param int           $page
+     * @param int           $limit
+     * @param null          $sort
+     * @param null          $direction
+     *
+     * @return StockResponse[]
+     */
+    public function listVariants(
+        array $articleCodes = null,
+        $articleDescription = null,
+        DateTime $updatedAfter = null,
+        $page = 1,
+        $limit = 1000,
+        $sort = null,
+        $direction = null
+    );
+
+    /**
      * @param string $articleCode
      * @param int    $minStock
      * @param int    $margin
